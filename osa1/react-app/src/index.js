391dc1ffde15e7bfc115fc1febbf602c62148df5
Markus Nivasalo <markus.nivasalo@outlook.com>
2020-12-18 14:10:21 +0200

    01 added

---
+++ osa1/react-app/src/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = (props) => {
+    return (
+        <>
+            <h1>{props.course}</h1>
+        </>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <>
+            <Part part={props.part1} exercises={props.amount1}/>
+            <Part part={props.part2} exercises={props.amount2}/>
+            <Part part={props.part3} exercises={props.amount3}/>
+        </>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <>
+            <p>
+                {props.part} {props.exercises}
+            </p>
+        </>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <>
+            <p>Number of exercises {props.amount1 + props.amount2 + props.amount3}</p>
+        </>
+    )
+}
+
+
+
+const App = () => {
+    const course = 'Half Stack application development';
+    const part1 = 'Fundamentals of React';
+    const exercises1 = 10;
+    const part2 = 'Using props to pass data';
+    const exercises2 = 7;
+    const part3 = 'State of a component';
+    const exercises3 = 14;
+
+    return (
+        <div>
+            <Header course={course}/>
+            <Content part1={part1} part2={part2} part3={part3} amount1={exercises1} amount2={exercises2} amount3={exercises3}/>
+            <Total amount1={exercises1} amount2={exercises2} amount3={exercises3}/>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />, document.getElementById('root')
+);
